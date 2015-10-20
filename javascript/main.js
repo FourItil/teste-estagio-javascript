98db7e6c98e104e307fcaf028659258f8f41d4b2
gabrielmedina <gmedina.santos@gmail.com>
2015-10-20 14:45:39 -0300

    Removendo itens explicativos do teste, deixando somente a resolução

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,30 @@
 $(document).ready(function(){
+  $('#campo-nome').keyup(function(){
+    /*
+      Resgatando o value do elemento cujo ID #campo-nome e adicionando o
+      resultado no elemento cujo ID #nome-digitado
+    */
+    $('#nome-digitado').text(
+      /*
+        $(this) faz referência ao elemento pai da função, cujo ID #campo-nome
+        .val() resgata o value do elemento
+      */
+      $(this).val()
+    );
 
-		$('#campo-nome').keyup(function(){
-			/*
-				Resgatando o value do elemento cujo ID #campo-nome e adicionando o
-			 	resultado no elemento cujo ID #nome-digitado
-			*/
-			$('#nome-digitado').text(
-				/*
-					$(this) faz referência ao elemento pai da função, cujo ID #campo-nome
-				 	.val() resgata o value do elemento
-				*/
-				$(this).val()
-			);
-
-			/*
-				Resgatando a quantidade de caracteres existentes no value do elemento
-				cujo ID #quantidade-caracteres e adicionando o resultado no elemento
-				cujo ID #quantidade-caracteres
-			*/
-			$('#quantidade-caracteres').text(
-				/*
-					$(this) faz referência ao elemento pai da função, cujo ID #campo-nome
-				 	.val() resgata o value do elemento
-					.length retorna a quantidade de caracteres do value
-				*/
-				$(this).val().length
-			);
-		});
+    /*
+      Resgatando a quantidade de caracteres existentes no value do elemento
+      cujo ID #quantidade-caracteres e adicionando o resultado no elemento
+      cujo ID #quantidade-caracteres
+    */
+    $('#quantidade-caracteres').text(
+      /*
+        $(this) faz referência ao elemento pai da função, cujo ID #campo-nome
+        .val() resgata o value do elemento
+        .length retorna a quantidade de caracteres do value
+      */
+      $(this).val().length
+    );
+  });
 
 });
